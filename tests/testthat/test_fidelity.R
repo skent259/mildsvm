363eff0d45641e4627e975e1cad8a2a1087bcdc5
Sean Kent <skent259@gmail.com>
2020-10-22 11:36:19 -0500

    update test_fidelity.R
    - add check fidelity of functions in misvm.R, cv_misvm.R

--- tests/testthat/test_fidelity.R
+++ tests/testthat/test_fidelity.R
@@ -2,6 +2,7 @@ context("Fidelity to MilDistribution package")
 suppressWarnings({
   library(mildsvm)
   library(MilDistribution)
+  library(dplyr)
 })
 
 
@@ -96,6 +97,58 @@ test_that("mil_distribution.R functions have identical output", {
 })
 
 test_that("misvm.R functions have identical output.", {
+  set.seed(8)
+  mil_data <- mildsvm::GenerateMilData(positive_dist = 'mvt',
+                                       negative_dist = 'mvnormal',
+                                       remainder_dist = 'mvnormal',
+                                       nbag = 10,
+                                       nsample = 7,
+                                       positive_degree = 3,
+                                       positive_prob = 0.15,
+                                       positive_mean = rep(0, 5))
+  df1 <- mildsvm::build_instance_feature(mil_data, seq(0.05, 0.95, length.out = 10))
+
+  set.seed(8)
+  mildsvm_output <- mildsvm::misvm(x = df1[, 4:123],
+                                   y = df1$bag_label,
+                                   bags = df1$bag_name,
+                                   cost = 1,
+                                   method = "heuristic")
+  set.seed(8)
+  MilDistribution_output <- MilDistribution::MI_SVM(df1, cost = 1)
+
+  mildsvm_output$model$call <- NULL
+  MilDistribution_output$svm_mdl$call <- NULL
+  expect_equal(mildsvm_output$model, MilDistribution_output$svm_mdl)
+  expect_equal(mildsvm_output$total_step, MilDistribution_output$total_step)
+  expect_equal(mildsvm_output$representative_inst, MilDistribution_output$representative_inst)
+
+  mildsvm_inst_pred <- df1 %>%
+    select(bag_label, bag_name) %>%
+    bind_cols(predict(mildsvm_output, new_data = df1, layer = "instance")) %>%
+    bind_cols(predict(mildsvm_output, new_data = df1, type = "raw", layer = "instance"))
+
+  mildsvm_bag_pred <- df1 %>%
+    select(bag_label, bag_name) %>%
+    bind_cols(predict(mildsvm_output, new_data = df1, type = "raw", layer = "bag")) %>%
+    bind_cols(predict(mildsvm_output, new_data = df1, layer = "bag")) %>%
+    distinct()
+
+  MilDistribution_pred <- predict(MilDistribution_output, newdata = df1)
+
+  expect_equivalent(
+    mildsvm_inst_pred,
+    MilDistribution_pred$instance_level_prediction
+  )
+  expect_equivalent(
+    mildsvm_bag_pred %>% select(-bag_label) %>% arrange(.pred),
+    MilDistribution_pred$bag_level_prediction %>% arrange(bag_score_pred)
+  )
+
+})
+
+test_that("cv_misvm.R functions have identical output.", {
+  set.seed(8)
   mil_data <- mildsvm::GenerateMilData(positive_dist = 'mvt',
                                        negative_dist = 'mvnormal',
                                        remainder_dist = 'mvnormal',
@@ -108,12 +161,42 @@ test_that("misvm.R functions have identical output.", {
 
 
   set.seed(8)
-  mildsvm_cv_output <- mildsvm::cv_MI_SVM(data = df1, n_fold = 3, cost_seq = 2^(-2:2))
+  mildsvm_cv_output <- mildsvm::cv_misvm(x = df1[, 4:123],
+                                         y = df1$bag_label,
+                                         bags = df1$bag_name,
+                                         cost_seq = 2^(-2:2),
+                                         n_fold = 3,
+                                         method = "heuristic")
   set.seed(8)
   MilDistribution_cv_output <- MilDistribution::cv_MI_SVM(df1, n_fold = 3, cost_seq = 2^(-2:2))
-  expect_equal(mildsvm_cv_output,
-               MilDistribution_cv_output)
 
+  mildsvm_cv_output$model$model$call <- NULL
+  MilDistribution_cv_output$BestMdl$svm_mdl$call <- NULL
+  expect_equal(mildsvm_cv_output$model$model, MilDistribution_cv_output$BestMdl$svm_mdl)
+  expect_equal(mildsvm_cv_output$model$total_step, MilDistribution_cv_output$BestMdl$total_step)
+  expect_equal(mildsvm_cv_output$model$representative_inst, MilDistribution_cv_output$BestMdl$representative_inst)
+
+  mildsvm_inst_pred <- df1 %>%
+    select(bag_label, bag_name) %>%
+    bind_cols(predict(mildsvm_cv_output, new_data = df1, layer = "instance")) %>%
+    bind_cols(predict(mildsvm_cv_output, new_data = df1, type = "raw", layer = "instance"))
+
+  mildsvm_bag_pred <- df1 %>%
+    select(bag_label, bag_name) %>%
+    bind_cols(predict(mildsvm_cv_output, new_data = df1, type = "raw", layer = "bag")) %>%
+    bind_cols(predict(mildsvm_cv_output, new_data = df1, layer = "bag")) %>%
+    distinct()
+
+  MilDistribution_pred <- predict(MilDistribution_cv_output$BestMdl, newdata = df1)
+
+  expect_equivalent(
+    mildsvm_inst_pred,
+    MilDistribution_pred$instance_level_prediction
+  )
+  expect_equivalent(
+    mildsvm_bag_pred %>% select(-bag_label) %>% arrange(.pred),
+    MilDistribution_pred$bag_level_prediction %>% arrange(bag_score_pred)
+  )
 
 })
 
